732b1ec487579a65a11feea5564c70bd619832ee
Sayanti Sarkar <68384931+sayantisarkar@users.noreply.github.com>
2021-03-22 11:42:57 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom"
+
+const date = new Date();
+const hours = date.getHours();
+
+ReactDOM.render(
+   
+   if(hours>=0 & hours<=12){
+    <h1>Good Morning</h1>
+   }
+  ,
+  document.getElementById("root")
+);
